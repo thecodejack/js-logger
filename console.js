1088fca9c7c47431cff56b6b9a18c282befdf3e0
thecodejack <aadisrikanth@gmail.com>
2014-01-02 10:03:05 +0000

    Beta Version

--- console.js
+++ console.js
@@ -1,3 +1,16 @@
+/*! Javascript logging library for jQuery 0.9.0 Beta
+ *
+ * Author: Adi Srikanth
+ * Dependencies: jquery > 1.6 version
+ * Free to use.
+ * Under no license.
+ */
+ 
+/*
+ * overrides native console object and pushes the logs to server
+ * currently pushes logs to server only when console.log() is called. 
+ */
+
 (function(){
     window.logger = $.extend(true,{}, console); //clone console object
     logger.c = console; //backup of console for methods which are overridden
@@ -20,6 +33,7 @@
     }
     logger.log = function(msg) {
         pushLogs(msg);
+        logger.c.log(msg);
     }
     window.console = logger;
 }());
